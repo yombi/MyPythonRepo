99a9354a5d124d29929756d30b9e31397a8f20af
yombi <yombiprimero@gmail.com>
2021-11-29 18:27:24 -0600

    Docummented algorithms

--- Algorithms/DP/Fibbo.py
+++ Algorithms/DP/Fibbo.py
@@ -1,13 +1,18 @@
 import sys
 
 def fibo(n,memo):
+    #We have already calculated the n possition of fibbonacci
     if n in memo:
         return memo[n]
+    #Base case needed in recursion
     if n<3:
         return 1
+    #Dinamic Programing: return the sum of the 2 previous possitions(by using fibo function) and save it. This is gonna return 1 for the first two possitions
     memo[n]=fibo(n-2,memo)+fibo(n-1,memo)
+    #Return the list with the fibbonacci serie
     return memo[n]
-
+#Defining dictionary
 memo={}
+#Parsing argument from command line
 n=int(sys.argv[1])
 print("Fibonacci at n={} is:\n{}".format(n,fibo(n,memo)))
--- Algorithms/DP/Traveler.py
+++ Algorithms/DP/Traveler.py
@@ -1,17 +1,25 @@
 import sys
 
 def travel(n,m,memo):
+    #Base case: n or m are zero
     if n*m==0:
         return 0
+    #Base case: We have a grid with one row or one column
     if n==1 or m==1:
         return 1
+    #If we have calculated the solution to a grid of n*m before, just return it
+    #Note that the solution to a grid of n*m is the same as one of m*n
     if (n,m) in memo or (m,n) in memo:
         return memo[(n,m)]
+    #Dinamic Programing: The arguments of travel() simulate a step taken to the right or down
     memo[(n,m)]=travel(n-1,m,memo)+travel(n,m-1,memo)
     memo[(m,n)]=travel(n-1,m,memo)+travel(n,m-1,memo)
+    #Return the dictonary with all the solutions to different calculated grids
     return memo[(n,m)]
 
+#Define dictonary
 memo={}
+#Parsing argument from command line
 n=int(sys.argv[1])
 m=int(sys.argv[2])
 print("For a grid of {}*{} the answer is:\n{}".format(n,m,travel(n,m,memo)))

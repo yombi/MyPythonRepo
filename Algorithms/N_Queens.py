5f6e56ddb6c6438aa8e01f8249d174481f54ffbc
yombi <yombiprimero@gmail.com>
2021-12-02 22:09:01 -0600

    Plot N-queen problem

--- Algorithms/N_Queens.py
+++ Algorithms/N_Queens.py
@@ -1,10 +1,17 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pylab as plt
 queens = 8
-#Defining the chesboard (queensXqueens)
-board=np.zeros([queens,queens],dtype=int).tolist()
+'''
+The solution is gonna be stored in a list and this list
+is going to be interpreted as follows:
+Let n be a column number of the chessboard,
+then solution[n] is the row number where the queen is
+placed for that n-th column
+'''
 solution = [0 for x in range(queens)]
 solutions = []
-
+pad="------"
 def safe(possible_x, possible_y):
     #First placed queen
     if possible_y == 0:
@@ -42,11 +49,21 @@ def placeQueenOnColumn(col):
                 #Move to the next column to check where to place the queen
                 placeQueenOnColumn(col + 1)
 
-print(f"Running the N-queens problem with {queens} queens")
+print(f"\n\t{pad}Running the N-queens problem with {queens} queens{pad}")
 placeQueenOnColumn(0)
-solution_number=int(input(f"There are {len(solutions)} solutions. Which solution number would you like to draw?"))
-i=0
-for row in solutions[solution_number]:
-    board[row][i]=1
-    i+=1
-print(np.matrix(board))
+print(f"\t{pad}There are {len(solutions)} solutions.{pad}")
+while True:
+    print(f"\t{pad}Input 0 to exit program{pad}")
+    solution_number=int(input(f"\t{pad}Which solution number would you like to draw?{pad}\nSolution Number:"))-1
+    if solution_number<0 or solution_number>92:
+        break
+    #Defining the chessboard (queensXqueens). Only 0
+    board=np.zeros([queens,queens],dtype=int).tolist()
+    i=0
+    #Parsing a solution contained in one of the lists in solutions to a 2d array
+    for row in solutions[solution_number]:
+        board[row][i]=1
+        i+=1
+    #Ploting
+    ax=sns.heatmap(board,linewidths=.8,cbar=False,linecolor='black',cmap='Reds',center=0.4)
+    plt.show()
